6172c1b4bd68fefdbe4d76373ea6e8a8b1d440bc
Oso Emmanuel <osoemmanuel1969@gmail.com>
2022-08-17 10:07:03 +0100

    more links for build

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -7,7 +7,9 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Admin</title>
 
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    {{-- @vite(['resources/sass/app.scss', 'resources/js/app.js']) --}}
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 
 <body>
@@ -29,13 +31,14 @@
                     @endguest
 
                     @auth
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="{{ route("admin.index") }}">Home</a>
-                    </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="{{ route('admin.index') }}">Home</a>
+                        </li>
 
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="{{ route("admin.message.index") }}">View all Message</a>
-                    </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="{{ route('admin.message.index') }}">View
+                                all Message</a>
+                        </li>
 
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
@@ -73,5 +76,9 @@
         @yield('content')
     </main>
 </body>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+</script>
 
 </html>
--- resources/views/layouts/user.blade.php
+++ resources/views/layouts/user.blade.php
@@ -7,7 +7,10 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>User</title>
 
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    {{-- @vite(['resources/sass/app.scss', 'resources/js/app.js']) --}}
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 
 <body>
@@ -33,13 +36,15 @@
 
                     @auth
                         <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="{{ route("user.home") }}">Home</a>
+                            <a class="nav-link active" aria-current="page" href="{{ route('user.home') }}">Home</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="{{ route("user.message.create") }}">Create Message</a>
+                            <a class="nav-link active" aria-current="page" href="{{ route('user.message.create') }}">Create
+                                Message</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="{{ route("user.message.index") }}">Show Message</a>
+                            <a class="nav-link active" aria-current="page" href="{{ route('user.message.index') }}">Show
+                                Message</a>
                         </li>
 
                         <li class="nav-item dropdown">
@@ -79,5 +84,9 @@
         @yield('content')
     </main>
 </body>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+</script>
 
 </html>
