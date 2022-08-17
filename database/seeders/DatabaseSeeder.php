dd575023cd1512520d0eb684390b042ef61f840e
Oso Emmanuel <osoemmanuel1969@gmail.com>
2022-08-17 11:03:15 +0100

    changed user email

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -34,7 +34,7 @@ class DatabaseSeeder extends Seeder
 
         Admin::create([
             'name' => "Admin",
-            'email' => "admin@admin.com",
+            'email' => "user@admin.com",
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
